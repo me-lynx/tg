9bee6a001f3175ab43e9ced8dc208e171aa19e0e
Maiza Louise <maizalouise@gmail.com>
2022-03-19 16:43:08 -0300

    Slidable + content

--- lib/master-user/list_vaccines_master.dart
+++ lib/master-user/list_vaccines_master.dart
@@ -1,6 +1,7 @@
 import 'package:dropdown_button2/custom_dropdown_button2.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 import 'package:tg/master-user/list_vaccines/list_vaccines_controller.dart';
 import 'package:tg/widgets/custom_app_bar.dart';
@@ -27,49 +28,9 @@ class _ListVaccinesMasterState extends State<ListVaccinesMaster> {
         slivers: <Widget>[
           _buildHeader(screenHeight),
           _content(screenHeight),
-          _content(screenHeight),
-          _content(screenHeight),
-          _content(screenHeight),
-          _content(screenHeight),
-          _content(screenHeight),
-          // Center(
-          //   child: DropdownButtonHideUnderline(
-          //     child: DropdownButton2(
-          //       hint: Expanded(
-          //         child: Text(
-          //           'Dr responsável',
-          //           style: TextStyle(fontSize: 13, color: Colors.white),
-          //         ),
-          //       ),
-          //       items: controller.items
-          //           .map((item) => DropdownMenuItem<String>(
-          //                 value: item,
-          //                 child: Text(
-          //                   item,
-          //                   style: const TextStyle(
-          //                     fontSize: 13,
-          //                   ),
-          //                 ),
-          //               ))
-          //           .toList(),
-          //       value: controller.selectedValue,
-          //       onChanged: (value) {
-          //         // setState(() {
-          //         //   selectedValue = value as String;
-          //         // });
-          //       },
-          //       icon: Icon(
-          //         Icons.search,
-          //         color: Colors.white,
-          //       ),
-          //       buttonHeight: 40,
-          //       buttonWidth: 140,
-          //       itemHeight: 40,
-          //     ),
-          //   ),
-          // ),
-          // _buildYourOwnTest(screenHeight),
-          // _buildYourOwnTest2(screenHeight),
+          _content2(screenHeight),
+          _content3(screenHeight),
+          _content4(screenHeight),
         ],
       ),
     );
@@ -82,7 +43,7 @@ SliverToBoxAdapter _content(double screenHeight) {
       padding: const EdgeInsets.all(25),
       child: Expanded(
         child: Container(
-          height: 17.h,
+          height: 20.h,
           decoration: BoxDecoration(
             color: Palette.primaryColor2,
             borderRadius: BorderRadius.all(
@@ -96,16 +57,20 @@ SliverToBoxAdapter _content(double screenHeight) {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Lote: 19209'),
+                    Text('Lote: 1920999'),
                     ClipRRect(
                       borderRadius: BorderRadius.all(Radius.circular(30)),
                       child: Card(
-                          child: Text(
-                            ' Pfizer ',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
+                          elevation: 3,
+                          child: Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: Text(
+                              ' Coronavac ',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
                             ),
                           ),
                           color: Palette.primaryColor),
@@ -150,6 +115,232 @@ SliverToBoxAdapter _content(double screenHeight) {
           ),
         ),
       ),
+    ),
+  );
+}
+
+SliverToBoxAdapter _content2(double screenHeight) {
+  return SliverToBoxAdapter(
+    child: Padding(
+      padding: const EdgeInsets.all(25),
+      child: Expanded(
+        child: Container(
+          height: 20.h,
+          decoration: BoxDecoration(
+            color: Palette.primaryColor2,
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Lote: 19209'),
+                    ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text(
+                              ' Pfizer ',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          color: Palette.primaryColor),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Validade: 12/12/2022'),
+                    SizedBox(),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('DR Responsável: Bauru - Vila Falcão'),
+                    SizedBox(),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  child: Card(
+                      child: Text(
+                        ' ATIVA ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                      color: Colors.green),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+SliverToBoxAdapter _content3(double screenHeight) {
+  return SliverToBoxAdapter(
+    child: Padding(
+      padding: const EdgeInsets.all(25),
+      child: Expanded(
+        child: Container(
+          height: 20.h,
+          decoration: BoxDecoration(
+            color: Palette.primaryColor2,
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Lote: 192349'),
+                    ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text(
+                              ' Janssen ',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          color: Palette.primaryColor),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Validade: 12/11/2022'),
+                    SizedBox(),
+                  ],
+                ),
+                SizedBox(
+                  height: 1.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('DR Responsável: Bauru - Carolina'),
+                    SizedBox(),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  child: Card(
+                      child: Text(
+                        ' ATIVA ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                      color: Colors.green),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+SliverToBoxAdapter _content4(double screenHeight) {
+  return SliverToBoxAdapter(
+    child: Slidable(
+      // Specify a key if the Slidable is dismissible.
+      key: const ValueKey(0),
+
+      // The start action pane is the one at the left or the top side.
+      startActionPane: ActionPane(
+        // A motion is a widget used to control how the pane animates.
+        motion: const ScrollMotion(),
+
+        // A pane can dismiss the Slidable.
+        dismissible: DismissiblePane(onDismissed: () {}),
+
+        // All actions are defined in the children parameter.
+        children: const [
+          // A SlidableAction can have an icon and/or a label.
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Share',
+          ),
+        ],
+      ),
+
+      // The end action pane is the one at the right or the bottom side.
+      endActionPane: const ActionPane(
+        motion: ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            flex: 2,
+            onPressed: null,
+            backgroundColor: Color(0xFF7BC043),
+            foregroundColor: Colors.white,
+            icon: Icons.archive,
+            label: 'Archive',
+          ),
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Color(0xFF0392CF),
+            foregroundColor: Colors.white,
+            icon: Icons.save,
+            label: 'Save',
+          ),
+        ],
+      ),
+
+      // The child of the Slidable is what the user sees when the
+      // component is not dragged.
+      child: const ListTile(title: Text('Slide me')),
     ),
   );
 }
--- lib/widgets/stats_grid.dart
+++ lib/widgets/stats_grid.dart
@@ -1,31 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:tg/widgets/custom_app_bar.dart';
+import 'package:tg/widgets/palete.dart';
 
 class StatsGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              child: Row(
-                children: <Widget>[
-                  _buildStatCard('Total Cases', '1.81 M', Colors.orange),
-                  _buildStatCard('Deaths', '105 K', Colors.red),
-                ],
-              ),
-            ),
-            Flexible(
-              child: Row(
-                children: <Widget>[
-                  _buildStatCard('Recovered', '391 K', Colors.green),
-                  _buildStatCard('Active', '1.31 M', Colors.lightBlue),
-                  _buildStatCard('Critical', 'N/A', Colors.purple),
-                ],
-              ),
-            ),
-          ],
-        ),
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _buildHeader(screenHeight),
+          _content(screenHeight),
+          _content2(screenHeight),
+        ],
       ),
     );
   }
@@ -33,6 +23,8 @@ class StatsGrid extends StatelessWidget {
   Expanded _buildStatCard(String title, String count, MaterialColor color) {
     return Expanded(
       child: Container(
+        height: 20.h,
+        width: 30.w,
         margin: const EdgeInsets.all(8.0),
         padding: const EdgeInsets.all(10.0),
         decoration: BoxDecoration(
@@ -43,12 +35,14 @@ class StatsGrid extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 15.0,
-                fontWeight: FontWeight.w600,
+            Center(
+              child: Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
             Text(
@@ -58,6 +52,80 @@ class StatsGrid extends StatelessWidget {
                 fontSize: 20.0,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildHeader(double screenHeight) {
+    String? selectedValue;
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Palette.primaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40.0),
+            bottomRight: Radius.circular(40.0),
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Status do Covid-19 em Bauru',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _content(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                _buildStatCard('Total de Casos', '1.81 M', Colors.orange),
+                _buildStatCard('Mortes', '105 K', Colors.red),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                _buildStatCard('Recuperados', '391 K', Colors.green),
+                _buildStatCard('Ativos', '1.31 M', Colors.lightBlue),
+                _buildStatCard('Criticos', '333', Colors.purple),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _content2(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                _buildStatCard('Vacinados', '3.4 K', Colors.pink),
+                _buildStatCard('Não vacinados', '105 K', Colors.grey),
+              ],
             ),
           ],
         ),
